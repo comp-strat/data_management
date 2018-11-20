24a56eea7415e6670a20299a21db280e2d90d309
Jaren Haber <jhaber@berkeley.edu>
2018-11-20 02:47:08 -0500

    fine-tuning comments

--- scripts/recover_leaid.py
+++ scripts/recover_leaid.py
@@ -2,37 +2,36 @@
 
 # Recover LEAID by geographical info
  
-# Authors: Ji Shi, Jiahua Zou, Jaren Haber
+# Authors: Jaren Haber, Ji Shi, Jiahua Zou
 # Institution: UC Berkeley
 # Contact: jhaber@berkeley.edu
  
 # Created: Nov. 12, 2018
 # Last modified: Nov. 19, 2018
 
-# Description: Geographically identifies each public school's LEAID (Local Education Agency Identification Code) by locating that school's latitude and longitude coordinates within school district shapefiles. Given that NCES school data lists for many schools--especially charter schools--an LEAID with legal but not geographic significance, this geographic matching allows analysis of each school within the community (school district) context in which it is physically situated. 
+# Description: Geographically identifies each public school's LEAID (Local Education Agency Identification Code) by locating that school's latitude and longitude coordinates within school district shapefiles. Given that NCES school data lists for many schools--especially charter schools--an LEAID with legal but not geographic significance, the geographic matching here allows analysis of each school within the community (school district) context in which it is physically and socially situated. 
 
 
 # Initialize
 
 import pandas as pd
-import numpy as np
-import os
 import gc # Makes loading pickle files faster 
 import sys # Script tricks (like exiting)
+import _pickle as cPickle # Optimized version of pickle
 
 from tqdm import tqdm # Shows progress over iterations, including in pandas via "progress_apply"
-tqdm.pandas(desc="Matching coords->LEAIDs") # To show progress, create & register new `tqdm` instance with `pandas`
+tqdm.pandas(desc="Matching coords-->LEAIDs") # To show progress, create & register new `tqdm` instance with `pandas`
 
 try:
     from shapely.geometry import Point, Polygon
 except ImportError:
-    get_ipython().system("pip install shapely # if module doesn't exist, then install")
+    get_ipython().system("pip install shapely") # if module doesn't exist, then install
     from shapely.geometry import Point, Polygon
 
 try:
     import geopandas as gpd
 except ImportError:
-    get_ipython().system("pip install geopandas # if module doesn't exist, then install")
+    get_ipython().system("pip install geopandas") # if module doesn't exist, then install
     import geopandas as gpd
 
 
@@ -70,7 +69,7 @@ def quickpickle_dump(dumpvar, picklepath):
     
 print("Loading data...")
 chartersdf = quickpickle_load("../../nowdata/charters_2015.pkl") # Charter school data (mainly NCES CCD PSUS, 2015-16)
-#pubsdf = quickpickle_load("../../nowdata/pubschools_2015.pkl") # Charter school data (mainly NCES CCD PSUS, 2015-16)
+pubsdf = quickpickle_load("../../nowdata/pubschools_2015.pkl") # Charter school data (mainly NCES CCD PSUS, 2015-16)
 sddata = quickpickle_load("../data/US_sd_combined_2016.pkl") # School district shapefiles (2016)
 acs = pd.read_csv("../data/ACS_2016_sd-merged_FULL.csv", header = [0, 1], encoding="latin1", low_memory=False) # School district social data (ACS, 2012-16)
 
@@ -96,26 +95,24 @@ def mapping_leaid(cord, original):
     
     global sdshapes, sdid # School district polygons, identifiers (equivalent to LEAID)
     
+    # If no coordinates, then can't match -> return original LEAID
     if not cord[0] or not cord[1]:
         return original
-    #print("Processing", cord)
 
-    for i in range(len(sdshapes)):
+    for i in range(len(sdshapes)): # Look at all school districts for matches
         
-        # Note: LAT & LON coordinates appear reversed when invoked with Point for gpd()
+        # Note: LAT & LON coordinates appear reversed when invoked with Point for gpd(), hence the order below
+        # Locate school's coordinates geographically within school district:
         if sdshapes[i].contains(Point((cord[1], cord[0]))): 
-            #print('Find a school district!')
-            if sdid.loc[i] != original:
-                #print(sdid.loc[i], "vs.", original, "- changed!")
-                return sdid.loc[i]
-            #print(sdid.loc[i], "vs.", original)
             return sdid.loc[i]
+        
+    # If no match, return original LEAID
     return original
 
 
 # Execute core function
 
-#pubsdf['GEO_LEAID'] = pubsdf[["LAT1516", 'LON1516','LEAID']].progress_apply(lambda x: mapping_leaid((x["LAT1516"], x['LON1516']), x['LEAID']), axis=1)
+pubsdf['GEO_LEAID'] = pubsdf[["LAT1516", 'LON1516','LEAID']].progress_apply(lambda x: mapping_leaid((x["LAT1516"], x['LON1516']), x['LEAID']), axis=1)
 chartersdf['GEO_LEAID'] = chartersdf[["LAT1516", 'LON1516','LEAID']].progress_apply(lambda x: mapping_leaid((x["LAT1516"], x['LON1516']), x['LEAID']), axis=1)
 
 
@@ -134,14 +131,14 @@ for var in list(acs):
 acs["GEO_LEAID"] = acs[("FIPS", "Geo_FIPS")] # Simplifies merging process
 chartersdf = chartersdf.drop(acsvars_ch, axis=1) # Drop ACS vars
 chartersdf = pd.merge(chartersdf, acs, how="left", on="GEO_LEAID")
-#pubsdf = pubsdf.drop(acsvars_pub, axis=1) # Drop ACS vars
-#pubsdf = pd.merge(pubsdf, acs, how="left", on="GEO_LEAID")
+pubsdf = pubsdf.drop(acsvars_pub, axis=1) # Drop ACS vars
+pubsdf = pd.merge(pubsdf, acs, how="left", on="GEO_LEAID")
 
 
 # Save modified data to disk
 
 print("Saving data to disk...")
-#quickpickle_dump(pubsdf, "../nowdata/backups/charters_full_2015_250_v2a_unlappedtext_counts3_geoleaid.pkl")
+quickpickle_dump(pubsdf, "../nowdata/backups/charters_full_2015_250_v2a_unlappedtext_counts3_geoleaid.pkl")
 quickpickle_dump(chartersdf, "../nowdata/backups/pubschools_full_2015_v2a_geoleaid.pkl")
 print("...done.")
 
