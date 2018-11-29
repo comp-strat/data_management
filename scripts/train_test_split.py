27f8dd474c7a4b9e3bcff83c4b79d907b03c8ab4
Jaren Haber <jhaber@berkeley.edu>
2018-11-29 08:16:29 -0500

    finalized filtering scripts

--- scripts/train_test_split.py
+++ scripts/train_test_split.py
@@ -6,8 +6,9 @@
 # Author: Jaren Haber, PhD Candidate
 # Project: Charter school identities
 # Institution: University of California, Berkeley, Dept. of Sociology
-# Date: August 27, 2018
-# This script splits the full charter data set (including school and community variables, including WEBTEXT) into a training set and test set using an 80/20 split (per Pareto). 
+# Date created: August 27, 2018
+# Date last modified: November 29, 2018
+# This script splits the full charter data set (including school and community variables, with WEBTEXT) into a training set and test set using an 80/20 split (as per Pareto). 
 
 
 ## Import key packages
@@ -18,25 +19,32 @@ from sklearn.model_selection import train_test_split # For splitting into train/
 import sys # For terminal tricks
 
 ## Define file paths
-charters_path = "../../nowdata/charters_full_2015_250_new_counts.pkl" # All text data; only charter schools (regardless if open or not)
-train_path = "../../nowdata/traincf_2015_250_new_counts.pkl" # Training set (80% of data)
-test_path = "../../nowdata/ignore/testcf_2015_250_new_counts.pkl"
+# Inputs:
+# Latest: "../../nowdata/backups/charters_full_2015_250_v2a_unlappedtext_counts3_geoleaid.pkl"
+charters_path = "../../nowdata/charters_2015.pkl" # All text data; only charter schools (regardless if open or not)
+stats_path = "../../nowdata/backups/charters_stats_2015_v2a.csv" # Quantitative data for statistics etc. (no text)
 
-# Load charter data into DF
+# Outputs:
+train_path = "../../nowdata/stats_traincf_2015_v2a.csv" # Training set (80% of data) for model development
+test_path = "../../nowdata/ignore/stats_testcf_2015_v2a.csv" # Test set (20%) for later use
+share_path = "../../wem4themes/charter_data_201516.csv" # For sharing data @ workshops, GitHub, etc.
+
+## Load charter data into DF
 gc.disable() # disable garbage collector
-df = pd.read_pickle(charters_path)
+df = pd.read_csv(stats_path, low_memory=False)
 gc.enable() # enable garbage collector again
 
-# Split data into 80% for training and 20% for test using random sample
+## Split data into 80% for training and 20% for test using random sample
 proptest = 0.2 # fraction of 0.2 = 20% test set
 print("Creating " + str((1-proptest)*100) + "/" + str(proptest*100) + " train/test split...")
 traindf, testdf = train_test_split(df, test_size = proptest, random_state = 0)
 
-# Save data for later use
-traindf.to_pickle(train_path)
-testdf.to_pickle(test_path)
+## Save data for later use
+traindf.to_csv(train_path, index=False)
+traindf.to_csv(share_path, index=False)
+testdf.to_csv(test_path, index=False)
 
-print("Training set saved to " + str(train_path))
+print("Training set saved to " + str(train_path) + " and " + str(share_path))
 print("Test set saved to " + str(test_path))
 
 sys.exit() # Kill script when done, just to be safe
