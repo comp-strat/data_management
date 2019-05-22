ec69894c5b2dde06591daf02bb9690076126c199
Jaren Haber <jhaber@berkeley.edu>
2019-05-22 07:22:20 -0400

    essential functions for creating and filtering data

---
+++ tools/clean_text.py
@@ -0,0 +1,202 @@
+# Import packages
+import re, datetime
+import string # for one method of eliminating punctuation
+from nltk.corpus import stopwords # for eliminating stop words
+from sklearn.feature_extraction import text
+from nltk.stem.porter import PorterStemmer; ps = PorterStemmer() # approximate but effective (and common) method of stemming words
+import os # for working with file trees
+
+# Prep dictionaries of English words
+from nltk.corpus import words # Dictionary of 236K English words from NLTK
+english_nltk = set(words.words()) # Make callable
+english_long = set() # Dictionary of 467K English words from https://github.com/dwyl/english-words
+fname =  "/vol_b/data/data_tools/english_words.txt" # Set file path to long english dictionary
+with open(fname, "r") as f:
+    for word in f:
+        english_long.add(word.strip())
+
+
+def stopwords_make(vocab_path_old = "", extend_stopwords = False):
+    """Create stopwords list. 
+    If extend_stopwords is True, create larger stopword list by joining sklearn list to NLTK list."""
+                                                     
+    stop_word_list = list(set(stopwords.words("english"))) # list of english stopwords
+
+    # Add dates to stopwords
+    for i in range(1,13):
+        stop_word_list.append(datetime.date(2008, i, 1).strftime('%B'))
+    for i in range(1,13):
+        stop_word_list.append((datetime.date(2008, i, 1).strftime('%B')).lower())
+    for i in range(1, 2100):
+        stop_word_list.append(str(i))
+
+    # Add other common stopwords
+    stop_word_list.append('00') 
+    stop_word_list.extend(['mr', 'mrs', 'sa', 'fax', 'email', 'phone', 'am', 'pm', 'org', 'com', 
+                           'Menu', 'Contact Us', 'Facebook', 'Calendar', 'Lunch', 'Breakfast', 
+                           'facebook', 'FAQs', 'FAQ', 'faq', 'faqs']) # web stopwords
+    stop_word_list.extend(['el', 'en', 'la', 'los', 'para', 'las', 'san']) # Spanish stopwords
+    stop_word_list.extend(['angeles', 'diego', 'harlem', 'bronx', 'austin', 'antonio']) # cities with many charter schools
+
+    # Add state names & abbreviations (both uppercase and lowercase) to stopwords
+    states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 
+              'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 
+              'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 
+              'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 
+              'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WI', 'WV', 'WY', 
+              'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 
+              'Colorado', 'Connecticut', 'District of Columbia', 'Delaware', 'Florida', 
+              'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 
+              'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 
+              'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 
+              'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 
+              'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 
+              'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 
+              'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 
+              'Vermont', 'Virginia', 'Washington', 'Wisconsin', 'West Virginia', 'Wyoming' 
+              'carolina', 'columbia', 'dakota', 'hampshire', 'mexico', 'rhode', 'york']
+    for state in states:
+        stop_word_list.append(state)
+    for state in [state.lower() for state in states]:
+        stop_word_list.append(state)
+        
+    # Add even more stop words:
+    if extend_stopwords == True:
+        stop_word_list = text.ENGLISH_STOP_WORDS.union(stop_word_list)
+        
+    # If path to old vocab not specified, skip last step and return stop word list thus far
+    if vocab_path_old == "":
+        return stop_word_list
+
+    # Add to stopwords useless and hard-to-formalize words/chars from first chunk of previous model vocab (e.g., a3d0, \fs19)
+    # First create whitelist of useful terms probably in that list, explicitly exclude from junk words list both these and words with underscores (common phrases)
+    whitelist = ["Pre-K", "pre-k", "pre-K", "preK", "prek", 
+                 "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", 
+                 "1st-grade", "2nd-grade", "3rd-grade", "4th-grade", "5th-grade", "6th-grade", 
+                 "7th-grade", "8th-grade", "9th-grade", "10th-grade", "11th-grade", "12th-grade", 
+                 "1st-grader", "2nd-grader", "3rd-grader", "4th-grader", "5th-grader", "6th-grader", 
+                 "7th-grader", "8th-grader", "9th-grader", "10th-grader", "11th-grader", "12th-grader", 
+                 "1stgrade", "2ndgrade", "3rdgrade", "4thgrade", "5thgrade", "6thgrade", 
+                 "7thgrade", "8thgrade", "9thgrade", "10thgrade", "11thgrade", "12thgrade", 
+                 "1stgrader", "2ndgrader", "3rdgrader", "4thgrader", "5thgrader", "6thgrader", 
+                 "7thgrader", "8thgrader", "9thgrader", "10thgrader", "11thgrader", "12thgrader"]
+    with open(vocab_path_old) as f: # Load vocab from previous model
+        junk_words = f.read().splitlines() 
+    junk_words = [word for word in junk_words[:8511] if ((not "_" in word) 
+                                                         and (not any(term in word for term in whitelist)))]
+    stop_word_list.extend(junk_words)
+                                                     
+    return stop_word_list
+                                                     
+    
+def punctstr_make():
+    """Creates punctuations list"""
+                    
+    punctuations = list(string.punctuation) # assign list of common punctuation symbols
+    #addpuncts = ['*','•','©','–','`','’','“','”','»','.','×','|','_','§','…','⎫'] # a few more punctuations also common in web text
+    #punctuations += addpuncts # Expand punctuations list
+    #punctuations = list(set(punctuations)) # Remove duplicates
+    punctuations.remove('-') # Don't remove hyphens - dashes at beginning and end of words are handled separately)
+    punctuations.remove("'") # Don't remove possessive apostrophes - those at beginning and end of words are handled separately
+    punctstr = "".join([char for char in punctuations]) # Turn into string for regex later
+
+    return punctstr
+                                                     
+                                                     
+def unicode_make():
+    """Create list of unicode chars"""
+                    
+    unicode_list  = []
+    for i in range(1000,3000):
+        unicode_list.append(chr(i))
+    unicode_list.append("_cid:10") # Common in webtext junk
+                                                     
+    return unicode_list
+
+
+def get_common_words(tokenized_corpus, max_percentage):
+    """Discover most common words in corpus up to max_percentage.
+    
+    Args:
+        Corpus tokenized by words,
+        Highest allowable frequency of documents in which a token may appear (e.g., 1-5%)
+        
+    Returns:
+        List of most frequent words in corpus"""
+    
+    # Code goes here
+    # Probably using nltk.CountVectorizer
+
+    
+# Create useful lists using above functions:
+stop_words_list = stopwords_make()
+punctstr = punctstr_make()
+unicode_list = unicode_make()
+
+def clean_sentence(sentence, remove_stopwords = True, keep_english = False, fast = False, exclude_words = [], stemming=False):
+    """Removes numbers, emails, URLs, unicode characters, hex characters, and punctuation from a sentence 
+    separated by whitespaces. Returns a tokenized, cleaned list of words from the sentence.
+    
+    Args: 
+        sentence, i.e. string that possibly includes spaces and punctuation
+        remove_stopwords: whether to remove stopwords, default True
+        keep_english: whether to remove words not in english dictionary, default False; if 'restrictive', keep word only if in NLTK's dictionary of 237K english words; if 'permissive', keep word only if in longer list of 436K english words
+        fast: whether to skip advanced sentence cleaning, removing emails, URLs, and unicode and hex chars, default False
+        exclude_words: list of words to exclude, may be most common words or named entities, default empty list
+        stemming: whether to apply PorterStemmer to each word, default False
+    Returns: 
+        Cleaned & tokenized sentence, i.e. a list of cleaned, lower-case, one-word strings"""
+    
+    global stop_words_list, punctstr, unicode_list, english_nltk, english_long
+    
+    # Replace unicode spaces, tabs, and underscores with spaces, and remove whitespaces from start/end of sentence:
+    sentence = sentence.replace(u"\xa0", u" ").replace(u"\\t", u" ").replace(u"_", u" ").strip(" ")
+    
+    if not fast:
+        # Remove hex characters (e.g., \xa0\, \x80):
+        sentence = re.sub(r'[^\x00-\x7f]', r'', sentence) #replace anything that starts with a hex character 
+
+        # Replace \\x, \\u, \\b, or anything that ends with \u2605
+        sentence = re.sub(r"\\x.*|\\u.*|\\b.*|\u2605$", "", sentence)
+
+        # Remove all elements that appear in unicode_list (looks like r'u1000|u10001|'):
+        sentence = re.sub(r'|'.join(map(re.escape, unicode_list)), '', sentence)
+    
+    sentence = re.sub("\d+", "", sentence) # Remove numbers
+    
+    sent_list = [] # Initialize empty list to hold tokenized sentence (words added one at a time)
+    
+    for word in sentence.split(): # Split by spaces and iterate over words
+        
+        word = word.strip() # Remove leading and trailing spaces
+        
+        # Filter out emails and URLs:
+        if not fast and ("@" in word or word.startswith(('http', 'https', 'www', '//', '\\', 'x_', 'x/', 'srcimage')) or word.endswith(('.com', '.net', '.gov', '.org', '.jpg', '.pdf', 'png', 'jpeg', 'php'))):
+            continue
+            
+        # Remove punctuation (only after URLs removed):
+        word = re.sub(r"["+punctstr+"]+", r'', word).strip("'").strip("-") # Remove punctuations, and remove dashes and apostrophes only from start/end of words
+        
+        if remove_stopwords and word in stop_words_list: # Filter out stop words
+            continue
+                
+        # TO DO: Pass in most_common_words to function; write function to find the top 1-5% most frequent words, which we will exclude
+        # Remove most common words:
+        if word in exclude_words:
+            continue
+            
+        if keep_english == 'restrictive':
+            if word not in english_nltk: #Filter out non-English words using shorter list
+                continue
+            
+        if keep_english == 'permissive': 
+            if word not in english_long: #Filter out non-English words using longer list
+                continue
+        
+        # Stem word (if applicable):
+        if stemming:
+            word = ps.stem(word)
+        
+        sent_list.append(word.lower()) # Add lower-cased word to list (after passing checks)
+
+    return sent_list # Return clean, tokenized sentence
---
+++ tools/train_test_split.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Train/test split
+
+# Author: Jaren Haber, PhD Candidate
+# Project: Charter school identities
+# Institution: University of California, Berkeley, Dept. of Sociology
+# Date created: August 27, 2018
+# Date last modified: November 29, 2018
+# This script splits the full charter data set (including school and community variables, with WEBTEXT) into a training set and test set using an 80/20 split (as per Pareto). 
+
+
+## Import key packages
+
+import pandas as pd # for working with dataframes
+import gc # For managing garbage collector (to increase efficiency loading large files into memory)
+from sklearn.model_selection import train_test_split # For splitting into train/test set
+import sys # For terminal tricks
+
+## Define file paths
+# Inputs:
+# Latest: "../../nowdata/backups/charters_full_2015_250_v2a_unlappedtext_counts3_geoleaid.pkl"
+charters_path = "../../nowdata/charters_2015.pkl" # All text data; only charter schools (regardless if open or not)
+stats_path = "../../nowdata/backups/charters_stats_2015_v2a.csv" # Quantitative data for statistics etc. (no text)
+
+# Outputs:
+train_path = "../../nowdata/stats_traincf_2015_v2a.csv" # Training set (80% of data) for model development
+test_path = "../../nowdata/ignore/stats_testcf_2015_v2a.csv" # Test set (20%) for later use
+share_path = "../../wem4themes/charter_data_201516.csv" # For sharing data @ workshops, GitHub, etc.
+
+## Load charter data into DF
+gc.disable() # disable garbage collector
+df = pd.read_csv(stats_path, low_memory=False)
+gc.enable() # enable garbage collector again
+
+## Split data into 80% for training and 20% for test using random sample
+proptest = 0.4 # fraction of 0.2 = 20% test set
+print("Creating " + str((1-proptest)*100) + "/" + str(proptest*100) + " train/test split...")
+traindf, testdf = train_test_split(df, test_size = proptest, random_state = 0)
+
+## Save data for later use
+traindf.to_csv(train_path, index=False)
+traindf.to_csv(share_path, index=False)
+testdf.to_csv(test_path, index=False)
+
+print("Training set saved to " + str(train_path) + " and " + str(share_path))
+print("Test set saved to " + str(test_path))
+
+sys.exit() # Kill script when done, just to be safe
